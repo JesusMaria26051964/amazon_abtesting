e8d52dcd3d734f9402aa06bde0d3f6578bbbd8b1
JesusMaria26051964 <jesusmaria26051964@outlook.es>
2026-01-27 12:41:57 +0100

    Cuarta subida

--- src/sp_eda.py
+++ src/sp_eda.py
@@ -4,6 +4,8 @@ df =pd.read_csv('../MODULO AB TESTING AMAZON/amazon_abtesting/data/data_raw.csv'
 
 print("Análisis Exploratorio de Datos Preliminar")
 
+eda_preliminar(df)
+
 def eda_preliminar(df):
     display(df.sample(5))
     print(f"Nuestro conjunto de datos tiene {df.shape[0]} filas y {df.shape[1]} columnas")
